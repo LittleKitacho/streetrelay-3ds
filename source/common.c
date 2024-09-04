c9ce8b961aa3772ecdbd53eaa81949abe61f1990
Daniel Budke <40306561+dabudke@users.noreply.github.com>
2024-09-04 16:30:27 -0400

    add API convenience functions
    todo: convert authentication and profile endpoints to use the new convention

--- source/common.c
+++ source/common.c
@@ -5,6 +5,7 @@
 #include <3ds.h>
 #include <curl/curl.h>
 #include "common.h"
+#include "cJSON.h"
 
 CURLcode init_request(CURL *curl, const char *url, const char *authHeader, struct curl_slist **headers)
 {
@@ -40,11 +41,12 @@ size_t downloadDataCallback(void *data, size_t size, size_t nmemb, void *handle)
   void *ptr = realloc(memory->buffer, memory->size + available + 1);
   if (ptr == NULL)
   {
-    printf("Ran out of memory while downloading data!");
+    printf("Ran out of memory while downloading data!\n");
     return 0;
   }
 
-  memcpy(&(memory->buffer[memory->size]), data, available);
+  memcpy(&(ptr[memory->size]), data, available);
+  memory->buffer = ptr;
   memory->size += available;
   memory->buffer[memory->size] = 0;
   return available;
@@ -62,4 +64,181 @@ void hang()
     gfxFlushBuffers();
     gfxSwapBuffers();
   }
+}
+
+typedef struct
+{
+  CURL *curl;
+  DownloadDataMemory data;
+} ApiRequestConfig;
+
+CURLcode apiInit(CURL *curl, const char *path, int pathLen, const char *authHeader, struct curl_slist **headers, DownloadDataMemory *download)
+{
+  *headers = curl_slist_append(*headers, authHeader);
+  *headers = curl_slist_append(*headers, "User-Agent: streetrelay-3ds/" VERSION);
+  *headers = curl_slist_append(*headers, "Accept: application/json");
+  *headers = curl_slist_append(*headers, "Accept: application/octet-stream");
+
+  // create full api path
+  int fullPathLen = sizeof(BASE_URL) + pathLen;
+  char *fullPath = malloc(fullPathLen + 1);
+  strncpy(fullPath, BASE_URL, fullPathLen);
+  strncat(fullPath, path, pathLen);
+
+  // setup data download object
+  download->buffer = malloc(1);
+  download->size = 0;
+
+  // setup curl object
+  CURLcode res = curl_easy_setopt(curl, CURLOPT_URL, fullPath);
+  free(fullPath);
+  if (res != CURLE_OK)
+    return res;
+  res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, *headers);
+  if (res != CURLE_OK)
+    return res;
+  res = curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+  if (res != CURLE_OK)
+    return res;
+  res = curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+  if (res != CURLE_OK)
+    return res;
+  res = curl_easy_setopt(curl, CURLOPT_TIMEOUT, 15L);
+  if (res != CURLE_OK)
+    return res;
+  res = curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
+  if (res != CURLE_OK)
+    return res;
+  res = curl_easy_setopt(curl, CURLOPT_WRITEDATA, download);
+  if (res != CURLE_OK)
+    return res;
+  res = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, downloadDataCallback);
+  if (res != CURLE_OK)
+    return res;
+}
+
+/**
+ * needs path to get from and auth header
+ * returns status code and json data
+ */
+CURLcode apiGet(const char *path, int pathLen, const char *authHeader, long *status, cJSON **response)
+{
+  // init curl
+  CURL *curl = curl_easy_init();
+  struct curl_slist *headers = NULL;
+  DownloadDataMemory download = {0, NULL};
+
+  CURLcode res = apiInit(curl, path, pathLen, authHeader, &headers, &download);
+  if (res != CURLE_OK)
+    goto cleanup;
+
+  res = curl_easy_perform(curl);
+  if (res != CURLE_OK)
+    goto cleanup;
+
+  // parse json response and extract status code
+  res = curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, status);
+  *response = cJSON_Parse(download.buffer);
+
+// cleanup
+cleanup:
+  free(download.buffer);
+  curl_easy_cleanup(curl);
+  curl_slist_free_all(headers);
+
+  return res;
+}
+
+CURLcode apiGetRaw(const char *path, int pathLen, const char *authHeader, long *status, void **response, size_t *responseSize)
+{
+  // init curl
+  CURL *curl = curl_easy_init();
+  struct curl_slist *headers = NULL;
+  DownloadDataMemory download = {0, NULL};
+
+  CURLcode res = apiInit(curl, path, pathLen, authHeader, &headers, &download);
+  if (res != CURLE_OK)
+    goto cleanup;
+
+  res = curl_easy_perform(curl);
+  if (res != CURLE_OK)
+  {
+    free(download.buffer);
+    goto cleanup;
+  }
+
+  res = curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, status);
+  *response = download.buffer;
+  *responseSize = download.size;
+
+cleanup:
+  curl_easy_cleanup(curl);
+  curl_slist_free_all(headers);
+
+  return res;
+}
+
+CURLcode apiPut(CURL *curl, curl_mime *mime, const char *path, int pathLen, const char *authHeader, long *status, cJSON **response)
+{
+  struct curl_slist *headers = NULL;
+  DownloadDataMemory download;
+
+  CURLcode res = apiInit(curl, path, pathLen, authHeader, &headers, &download);
+  if (res != CURLE_OK)
+    goto cleanup;
+
+  res = curl_easy_setopt(curl, CURLOPT_MIMEPOST, mime);
+  if (res != CURLE_OK)
+    goto cleanup;
+  res = curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
+  if (res != CURLE_OK)
+    goto cleanup;
+
+  res = curl_easy_perform(curl);
+  if (res != CURLE_OK)
+    goto cleanup;
+
+  if (status != NULL)
+    res = curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, status);
+  if (response != NULL)
+    *response = cJSON_ParseWithLength(download.buffer, download.size);
+
+cleanup:
+  free(download.buffer);
+  curl_easy_cleanup(curl);
+  curl_mime_free(mime);
+  curl_slist_free_all(headers);
+
+  return res;
+}
+
+CURLcode apiDelete(const char *path, int pathLen, const char *authHeader, long *status, cJSON **response)
+{
+  // todo
+  CURL *curl = curl_easy_init();
+  struct curl_slist *headers = NULL;
+  DownloadDataMemory download = {0, NULL};
+
+  CURLcode res = apiInit(curl, path, pathLen, authHeader, &headers, &download);
+  if (res != CURLE_OK)
+    goto cleanup;
+
+  res = curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+  if (res != CURLE_OK)
+    goto cleanup;
+
+  res = curl_easy_perform(curl);
+  if (res != CURLE_OK)
+    goto cleanup;
+
+  res = curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, status);
+  *response = cJSON_Parse(download.buffer);
+
+// cleanup
+cleanup:
+  free(download.buffer);
+  curl_easy_cleanup(curl);
+  curl_slist_free_all(headers);
+
+  return res;
 }
--- source/common.h
+++ source/common.h
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <3ds.h>
 #include <curl/curl.h>
+#include "cJSON.h"
 
-#define BASE_URL "http://192.168.68.117:5173"
+#define BASE_URL "http://172.20.10.11:5173"
 #define VERSION "1.0.0"
 
 typedef struct _DownloadDataMemory
@@ -14,5 +15,10 @@ typedef struct _DownloadDataMemory
 CURLcode init_request(CURL *curl, const char *path, const char *authHeader, struct curl_slist **headers);
 
 size_t downloadDataCallback(void *data, size_t size, size_t nmemb, void *handle);
+
+CURLcode apiGet(const char *path, int pathLen, const char *authHeader, long *status, cJSON **response);
+CURLcode apiGetRaw(const char *path, int pathLen, const char *authHeader, long *status, void **response, size_t *responseSize);
+CURLcode apiPut(CURL *curl, curl_mime *mime, const char *path, int pathlen, const char *authHeader, long *status, cJSON **response);
+CURLcode apiDelete(const char *path, int pathLen, const char *authHeader, long *status, cJSON **response);
 
 void hang();
